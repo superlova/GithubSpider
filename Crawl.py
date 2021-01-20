05aae65e691e502237d2f015cce4fdb9d3fecdc6
superlova <49007965+superlova@users.noreply.github.com>
2021-01-20 14:40:39 +0800

    完成爬取、比较和储存的步骤，并利用多进程实现

--- Crawl.py
+++ Crawl.py
@@ -11,9 +11,13 @@ import json
 from concurrent.futures import ProcessPoolExecutor
 import logging
 import pandas as pd
-from fake_useragent import UserAgent
 import configparser
+import difflib
 from Utils import HEADERS, make_interval, log_tracer
+from Utils import delete_comment_and_docstrings
+from Utils import get_diff_file
+from Utils import b2t, timethis
+
 
 
 class GiuHubCommitCrawer(object):
@@ -136,10 +140,108 @@ class GiuHubCommitCrawer(object):
 
 
 class GitHubCompare(object):
-    def __init__(self, pickle_filename):
-        self.df = pd.read_pickle(pickle_filename)
+    def __init__(self, user, repo, pickle_filename, token):
+        self.user = user
+        self.repo = repo
+        df = pd.read_pickle(pickle_filename)
+        self.df = df[df['status'] == 'modified']  # commit有四个类型，只有modified是前后变动的
+        print(self.df)
 
+        self.token = token
 
+        self.contents_template = "https://api.github.com/repos/tensorflow/tensorflow/contents/{filename}?ref={sha}"
+        self.limit_url = "https://api.github.com/rate_limit"  # 查询还剩多少次访问github的url
+
+        self.headers = HEADERS
+        self.headers['Authorization'] = "token " + token
+
+        self.minus_lines = []  # 爬到的所有单行注释。后来想想没用，因为我们需要上下文信息。
+
+    def check_remaining(self):
+        """查询还剩多少次访问github的机会，一个账号一个小时允许5000次
+        返回剩余次数"""
+        text = self.get_html_content(self.limit_url)
+        text_json = json.loads(text)
+        self.rate_limit = text_json["rate"]["limit"]
+        rate_remaining = text_json["rate"]["remaining"]
+        print("limit rate: {}".format(self.rate_limit))
+        print("remaining rate: {}".format(rate_remaining))
+        return rate_remaining
+
+    def get_html_content(self, url, retry=5, params=None):
+        """直接获取url的内容，出现错误就最多重试5次
+        如果没有剩余查询次数，就返回中括号字符串，因为返回值要输入json，所以不能直接返回空值"""
+        while retry > 0:
+            try:
+                logging.info(f"Crawing {url}")
+                r = requests.get(url, params, headers=self.headers, timeout=10)
+                r.raise_for_status()
+                r.encoding = r.apparent_encoding
+                return r.text
+            except requests.exceptions.ReadTimeout:
+                logging.info("ReadTimeOut!")
+                retry -= 1
+            except requests.exceptions.ConnectionError:
+                logging.info("ConnectionTimeOut!")
+                retry -= 1
+            except requests.exceptions.HTTPError as e:
+                logging.info("HTTPError!, {}, {}".format(type(e), str(e)))
+                retry -= 1
+                if self.check_remaining() == 0:
+                    print("No remaining rate! exit.")
+                    return "[]"  # if len(json.loads(get_html_content(url))) == 0: return
+
+    def get_file_content(self, filename, sha):
+        """给定文件目录和sha，爬取对应文件的base64编码后的内容"""
+        url = self.contents_template.format(filename=filename, sha=sha)
+        text = self.get_html_content(url=url)
+        content = json.loads(text)
+        if len(content) == 0:
+            return ""
+        return content.get('content')
+
+    @timethis
+    def traverse_df_add_columns(self):
+        """增加两列，用于储存爬取完毕的commit文件和父提交文件"""
+        self.df['origin_content'] = self.df.apply(
+            lambda row: self.get_file_content(row['filename'], row['sha']),
+            axis=1)
+        self.df['parent_content'] = self.df.apply(
+            lambda row: self.get_file_content(row['filename'], row['parents_sha']),
+            axis=1)
+        self.df['pure_code'] = self.df.apply(
+            lambda row: get_diff_file(b2t(row['parent_content']), b2t(row['origin_content'])),
+            axis=1)
+
+    def save_df(self, filename):
+        self.df.to_pickle(f"{filename}.tar.gz")
+
+    ##########################
+
+    def get_diff(self, origin_file, parent_file):
+        """比较输入的两份文本文件的差距行，把差距储存到self.minus_lines里面
+        这种做法忽略了上下文，因此不可取"""
+        origin_file_lines = origin_file.splitlines()
+        parent_file_lines = parent_file.splitlines()
+        d = difflib.Differ()
+        diff = d.compare(parent_file_lines, origin_file_lines)
+        minus_lines = []
+        for line in list(diff):
+            if line.startswith('-') and len(line) > 2:  # 排除空行，因为空行只包括两个符号：减号与空格
+                minus_lines.append(line.lstrip('- '))
+        return minus_lines
+
+    def traverse_df_find_minus(self):
+        for index, row in self.df.iterrows():
+            origin_file = self.get_file(filename=row['filename'], sha=row['sha'])
+            parent_file = self.get_file(filename=row['filename'], sha=row['parents_sha'])
+            self.minus_lines.extend(self.get_diff(origin_file, parent_file))
+            logging.info("total minus_lines: {}".format(len(self.minus_lines)))
+
+    def save_minus_file(self, filename):
+        with open(f"{filename}.txt", 'wt') as f:
+            for line in self.minus_lines:
+                f.write(line + '\n')
 
 
 def test_github_crawler():
@@ -178,7 +280,6 @@ def test_load_df():
     df = pd.read_pickle("tensorflow-tensorflow-SHAs.tar.bz2")
     print(df.iloc[0]['patch'])
 
-
 def test_token():
     gc = GithubCommentCrawer('tensorflow', "tensorflow")
 
@@ -196,9 +297,31 @@ def test_end():
     content = json.loads(text)
     print(len(content))
 
+def test_github_compare(i):
+    config = configparser.ConfigParser()
+    config.read(os.path.abspath('config_compare.ini'))
+    group = []
+    for section in config.sections():
+        di = {}
+        for item in config.items(section):
+            di[item[0]] = item[1]
+        group.append(di)
+
+    item = group[i]
+    gc = GitHubCompare(user=item['user'], repo=item['repo'], token=item['token'], pickle_filename=item['pickle_filename'])
+    gc.traverse_df_add_columns()
+    gc.save_df(item['pickle_filename'].split('.')[0])
+
+def test_multiprocessing_compare():
+    with ProcessPoolExecutor() as pool:
+        pool.map(test_github_compare, [0,1,2,3])
+
+
+
 
 def main():
     logging.basicConfig(
+        filename="crawl.log",
         level=logging.INFO
     )
     # test_token()
@@ -206,7 +329,8 @@ def main():
     # test_load_df()
     # test_end()
     # test_github_crawer_new(3)
-    test_ProcessPoolExecutor()
+    # test_ProcessPoolExecutor()
+    test_multiprocessing_compare()
 
 
 if __name__ == '__main__':
